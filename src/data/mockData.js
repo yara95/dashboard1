bb4f42ff4b900c3923b1de6239053d32b9c4568b
Yara <yara.kahmad.95@gmail.com>
2023-02-28 18:45:47 +0400

    v1.1

---
+++ src/data/mockData.js
@@ -0,0 +1,20 @@
+export const mockDataTeam = [
+    {
+      id: 1,
+      name: "yara ahmad",
+      email: "yara@gmail.com",
+      age: 35,
+      phone: "0000000",
+      access: "admin",
+    },
+    {
+      id: 2,
+      name: "luna",
+      email: "luna@gmail.com",
+      age: 42,
+      phone: "0000000",
+      access: "manager",
+    },
+    
+  ];
+  
